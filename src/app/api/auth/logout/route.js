1a559134233e96b9a5093c5dbb0e786cfbaf33a4
lucas.lnrr <lucas.lnrr@gmail.com>
2025-09-04 21:16:00 -0300

    logout fix

--- src/app/api/auth/logout/route.js
+++ src/app/api/auth/logout/route.js
@@ -1,13 +1,15 @@
 export const runtime = 'nodejs'
 export const dynamic = 'force-dynamic'
 import { NextResponse } from 'next/server'
+
 export async function POST(req){
-  const res = NextResponse.redirect(new URL('/login', req.url))
+  const res = NextResponse.redirect(new URL('/login', req.url), 303) // <- force GET
   res.cookies.set('token','', { httpOnly:true, secure:true, sameSite:'lax', path:'/', expires: new Date(0) })
   return res
 }
+
 export async function GET(req){
-  const res = NextResponse.redirect(new URL('/login', req.url))
+  const res = NextResponse.redirect(new URL('/login', req.url), 303)
   res.cookies.set('token','', { httpOnly:true, secure:true, sameSite:'lax', path:'/', expires: new Date(0) })
   return res
 }
